c92731a2b50d277bc47f2ff8f58335991877279f
Unknown <sanchez.dav90@gmail.com>
2018-01-31 21:29:24 -0600

    Devise registered and unregistered roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+
+  authenticated :user do
+    root 'home#index'
+  end
+
+  unauthenticated :user do
+    root 'home#unregistered'
+  end
 end
