fe4e62d1ffd63129d3bdaa7ca7687ea2da9b6336
Unknown <sanchez.dav90@gmail.com>
2018-01-31 22:19:31 -0600

    Custom signup view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    :omniauth_callbacks => "users/omniauth_callbacks"
+  }
+
+  post "/sign_up_validation", to: "users/omniauth_callbacks#sign_up_validation"
 
   authenticated :user do
     root 'home#index'
