a844a6af712943ea6690e3aae44c728d073535be
Unknown <sanchez.dav90@gmail.com>
2018-02-03 23:16:15 -0600

    User registration path without password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout "application", only: [:edit, :update]
+
+  protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    edit_user_registration_path
+  end
 end
