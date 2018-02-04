bb38e04e68f5bf6006970d29b2784308bec6bc25
Unknown <sanchez.dav90@gmail.com>
2018-02-03 23:44:04 -0600

    Basic Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :name, :content, presence: true
+  validates :name, length: { minimum: 2, maximum: 50 }
+  validates :content, length: { minimum: 2, maximum: 250 }
 end
