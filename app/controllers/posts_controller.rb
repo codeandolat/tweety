d2b9bcd4c202b7fa429d0fa9f0d62e38c93e1206
Unknown <sanchez.dav90@gmail.com>
2018-02-04 23:25:50 -0600

    Post index added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.eager_load(:user).order(created_at: :desc)
   end
 
   # GET /posts/1
