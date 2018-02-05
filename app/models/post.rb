ddd0af81a8fa6d8dfa366cdcfbc17ce1f5e2fdaa
Unknown <sanchez.dav90@gmail.com>
2018-02-04 23:25:39 -0600

    Post validation changed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :name, :content, presence: true
-  validates :name, length: { minimum: 2, maximum: 50 }
-  validates :content, length: { minimum: 2, maximum: 250 }
+  validates :name, length: { minimum: 2, maximum: 20 }
+  validates :content, length: { minimum: 2, maximum: 120 }
 end
