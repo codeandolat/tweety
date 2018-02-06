00d1e6b2576821a32a293fb74834e3ffbc8970db
Unknown <sanchez.dav90@gmail.com>
2018-02-05 20:31:46 -0600

    User only can edit his posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
 
   # GET /posts
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.friendly.find(params[:id])
   end
 
   # GET /posts/new
@@ -65,7 +66,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.friendly.find(params[:id])
+      @post = current_user.posts.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
