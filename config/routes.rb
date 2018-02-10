4a7d564d008354db5f3ee830f56f7712d0f0a596
Unknown <sanchez.dav90@gmail.com>
2018-02-10 00:58:31 -0600

    Comments controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts do
+    resources :comments, only: [:create, :destroy]
     put "like", to: "posts#like"
   end
   post "/sign_up_validation", to: "users/omniauth_callbacks#sign_up_validation"
